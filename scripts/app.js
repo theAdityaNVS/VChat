73d3830316341a2583caec4ecfb27ce6d8bb5518
theAdityaNVS <aditya.nadamuni@gmail.com>
2021-07-23 17:02:20 +0530

    added message function

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,15 @@
 const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+
+newChatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message = newChatForm.message.value.trim();
+    chatroom.addChat(message).then(() => {
+        newChatForm.reset();
+    }).catch((er) => {
+        console.log(er);
+    })
+})
 
 const chatUI = new ChatUI(chatList);
 const chatroom = new Chatroom('general', 'shaun');
