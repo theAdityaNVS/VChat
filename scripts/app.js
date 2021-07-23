d47593befd853e3989b717939d7411e317e1183d
theAdityaNVS <aditya.nadamuni@gmail.com>
2021-07-23 21:11:20 +0530

    added change room and localstorage

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,8 @@
 const chatList = document.querySelector('.chat-list');
 const newChatForm = document.querySelector('.new-chat');
+const newNameForm = document.querySelector('.new-name');
+const updateMsg = document.querySelector('.update-msg');
+const rooms = document.querySelector('.chat-rooms');
 
 newChatForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -11,8 +14,30 @@ newChatForm.addEventListener('submit', (e) => {
     })
 })
 
+newNameForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const newName = newNameForm.name.value.trim();
+    chatroom.updateName(newName);
+    newNameForm.reset();
+    updateMsg.innerHTML = `Name has been updated to ${newName}`;
+    setTimeout(() => {
+        updateMsg.innerHTML = '';
+    }, 3000);
+});
+
+rooms.addEventListener('click', (e) => {
+    // console.log(e);
+    if (e.target.tagName === 'BUTTON') {
+        chatUI.clear();
+        chatroom.updateRoom(e.target.getAttribute('id'));
+        chatroom.getChats(chat => chatUI.render(chat));
+    }
+});
+
+const username = localStorage.username ? localStorage.username : 'Anonymous';
+
 const chatUI = new ChatUI(chatList);
-const chatroom = new Chatroom('general', 'shaun');
+const chatroom = new Chatroom('general', username);
 
 chatroom.getChats((data) => {
     // console.log(data);
