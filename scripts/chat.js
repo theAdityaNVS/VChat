b23d1950706c4dbcb545ab05f05824e448517bd8
theAdityaNVS <aditya.nadamuni@gmail.com>
2021-07-23 14:53:15 +0530

    added change room and name

--- scripts/chat.js
+++ scripts/chat.js
@@ -3,6 +3,7 @@ class Chatroom {
         this.room = room;
         this.username = username;
         this.chats = db.collection('chats');
+        this.unsub;
     }
     async addChat(message) {
         const now = new Date();
@@ -16,13 +17,24 @@ class Chatroom {
         return response;
     }
     getChats(callback) {
-        this.chats.onSnapshot(snapshot => {
-            snapshot.docChanges().forEach(change => {
-                if (change.type === 'added') {
-                    callback(change.doc.data());
-                }
-            })
-        });
+        this.unsub = this.chats.where('room', '==', this.room)
+            .orderBy('created_at')
+            .onSnapshot(snapshot => {
+                snapshot.docChanges().forEach(change => {
+                    if (change.type === 'added') {
+                        callback(change.doc.data());
+                    }
+                })
+            });
+    }
+    updateName(username) {
+        this.username = username;
+    }
+    updateRoom(room) {
+        this.room = room;
+        console.log('room updated');
+        if (this.unsub);
+        this.unsub();
     }
 }
 
@@ -34,3 +46,13 @@ const chatroom = new Chatroom('gaming', 'shaun')
 chatroom.getChats((data) => {
     console.log(data);
 })
+
+// chatroom.updateRoom('general');
+setTimeout(() => {
+    chatroom.updateRoom('general');
+    chatroom.updateName('yoshi');
+    chatroom.getChats((data) => {
+        console.log(data)
+    });
+    chatroom.addChat('hello');
+}, 3000);
